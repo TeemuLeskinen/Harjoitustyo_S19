e23d1cf59fb40b627f9c0ce07e3ee9982206240c
Teemu Leskinen <t8lete03@students.oamk.fi>
2019-09-24 17:50:36 +0300

    menu.php valmis

--- menu.php
+++ menu.php
@@ -12,11 +12,19 @@
         </div>
 
         <div id="menu">
-        Menu        
+         <ul>
+            <li><a href="index.php">Main page</a></li>
+            <li><a href="snowboards.php">Snowboards</a></li>
+            <li><a href="boots.php">Snowboard Boots</a></li>
+            <li><a href="bindings.php">Snowboard Bindings</a></li>
+            <li><a href="shoppingcart.php">Shopping Cart</a></li>
+		
+	     </ul>
+	        
         </div>
         
-        <div id="menu">
-        Menu        
+        <div id="content">
+        This is the content        
         </div>
         
         <div id="footer">
