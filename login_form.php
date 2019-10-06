d766e67749c7d72f2d48dfbb817516ea13620e4d
Teemu Leskinen <t8lete03@students.oamk.fi>
2019-10-06 15:00:19 +0300

    korjaus

--- login_form.php
+++
@@ -1,8 +0,0 @@
-<?php include "menu.php"; ?>
-<form class="" action="login.php" method="post">
-    <input type="text" name="username" value="" placeholder="Username"> <br> <br>
-    <input type="text" name="password" value="" placeholder="Password"> <br> <br>
-    <input type="submit" name="" value="Login">
-</form>
-<?php include "footer.php"; ?>
-    
