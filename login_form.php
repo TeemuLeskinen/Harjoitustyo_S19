2e48b7914fe6e415cb2f18dcdd2cc7b4b9f27ae0
Teemu Leskinen <t8lete03@students.oamk.fi>
2019-10-06 14:46:58 +0300

    tietoja lisätty

---
+++ login_form.php
@@ -0,0 +1,8 @@
+<?php include "menu.php"; ?>
+<form class="" action="login.php" method="post">
+    <input type="text" name="username" value="" placeholder="Username"> <br> <br>
+    <input type="text" name="password" value="" placeholder="Password"> <br> <br>
+    <input type="submit" name="" value="Login">
+</form>
+<?php include "footer.php"; ?>
+    
---
+++ shoppingcart.php
@@ -0,0 +1,10 @@
+<?php include "menu.php"; ?>
+	<h2>Shopping Cart</h2>
+	<?php	
+
+		//echo '<p> Your Shopping Cart:</p>' .$_POST['bindings'];
+		echo '<p> Your Shopping Cart:</p>';
+		print_r($_POST);
+	
+	?>
+	<?php include "footer.php"; ?>
